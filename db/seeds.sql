91803d1cbfab16dca42bc69a1d41ac99a6e64302
Steffi Joel <steffijerome0809>
2020-06-24 01:25:18 -0400

    added sql files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Table Values -- 
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Single Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("Double Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
+INSERT INTO burgers (burger_name) VALUES ("Western Burger");
