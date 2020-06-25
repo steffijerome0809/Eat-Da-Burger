cd101ba83c0d746d734fec577e8107e0c72f91ea
Steffi Joel <steffijerome0809>
2020-06-25 00:07:03 -0400

    adding jawsdb update

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  });
+}
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("db connected");
+});
+
+module.exports = connection;
