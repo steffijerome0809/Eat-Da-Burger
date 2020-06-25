f4f509660843b4e0df53e32cd68958f23ae35fcd
Steffi Joel <steffijerome0809>
2020-06-25 16:40:54 -0400

    changes to serverjs file

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "burgers_db",
-  });
-}
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db",
+});
+// }
 
 connection.connect(function (err) {
   if (err) {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // requiring dependencies
-var express = require("express");
-
+const express = require("express");
+const exphbs = require("express-handlebars");
 //port config
 var PORT = process.env.PORT || 8080;
 var app = express();
@@ -13,13 +13,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //set handlebars
-var exphbs = require("express-handlebars");
+
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // import routes
-const routes = require("./controllers/burger_controller.js");
-app.use(routes);
+// const routes = require("./controllers/burger_controller.js");
+// app.use(routes);
 
 //start the server
 app.listen(PORT, function () {
