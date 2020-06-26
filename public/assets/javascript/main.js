187acbc6c95a48da23e4067591e97a1d10dfec3b
Steffi Joel <steffijerome0809>
2020-06-26 02:06:40 -0400

    added main.js

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -1,8 +1,39 @@
 // function to house both the on-click functions-dom must be loaded
 $(function () {
+  // on click function next to the burger names
+  $(".changeDevoured").on("click", function (event) {
+    var id = $(this).data("id");
+    var devouredstate = $(this).data("devouredstate");
+
+    var newDevouredState = {
+      devoured: devouredstate,
+    };
+    //send the put request
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState,
+    }).then(function () {
+      //reload
+      location.reload();
+    });
+  });
   //on click function that creates a burger
 
   $("#makeBurger").on("click", function (event) {
     event.preventDefault();
+
+    var newBurger = {
+      name: $("#newBurgerName").val().trim(),
+      devoured: false,
+    };
+    //send the post req
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("created new burger");
+      //reload the page to get the updated list
+      location.reload();
+    });
   });
 });
