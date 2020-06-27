f4868cafb4c724d22650418e5e669cd7028e91d1
Steffi Joel <steffijerome0809>
2020-06-26 22:50:56 -0400

    changes to contoller.js with the insert and update

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -12,6 +12,31 @@ router.get("/", function (req, res) {
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
+
+  // post route which will be used to create a new burger
+  router.post("/api/burgers", function (req, res) {
+    burger.create(req.body.name, function (data) {
+      res.json({ id: data.insertId });
+    });
+  });
+
+  //update req-put
+  router.put("/api/burgers/:id", function (req, res) {
+    var updateid = "id = " + req.params.id;
+    burger.update(
+      {
+        devoured: req.body.devoured,
+      },
+      updateid,
+      function (result) {
+        if (result.changedRows == 0) {
+          return res.status(404).end();
+        } else {
+          res.status(200).end();
+        }
+      }
+    );
+  });
 });
 
 module.exports = router;
